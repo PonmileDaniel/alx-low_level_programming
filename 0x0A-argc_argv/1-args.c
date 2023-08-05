a240c21d1813e333e79b91b8c81a300b80f9fa7f
PonmileDaniel <pomiledaniel@gmail.com>
2023-08-05 12:23:41 -0700

    ./nargs hello

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of arguement
+ * @argc: arguement count
+ * @argv: arguement vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
