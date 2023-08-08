5e8d4bbd2899b03929a4a14745512b273b463ced
PonmileDaniel <pomiledaniel@gmail.com>
2023-08-08 06:12:53 -0700

    ./a

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - functhat create an array of chars .
+ * @size: size of array
+ * @c: character to fill
+ * Return: chars
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		p = malloc(sizeof(char) * size);
+	}
+
+	if (p == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			*(p + i) = c;
+		}
+	}
+	return (p);
+}
