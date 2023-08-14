899124a181e0e3842799b284c79e6121bdba80cc
PonmileDaniel <pomiledaniel@gmail.com>
2023-08-14 15:29:47 -0700

    ./c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - To print the name of the current file
+ * Return: 0 To print the success
+ */
+
+int main(void)
+{
+	printf("File = %s\n", __FILE__);
+
+	return (0);
+}
