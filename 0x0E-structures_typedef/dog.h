300988806368d615513ef2f7f02f444e0be6bbf7
PonmileDaniel <pomiledaniel@gmail.com>
2023-08-15 13:21:38 -0700

    ./a

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Struct for Dog
+ * @name: Name of DOG
+ * @age: Age of Dog
+ * @owner: owner of DOG
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
