c8478dff9da24f9422a53431ea70df32324143b9
Ponmile Daniel <pomiledaniel@gmail.com>
2023-08-25 15:19:44 -0700

    jj

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,18 +12,20 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-	size_t nchar;
+	size_t i;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
+	{
 		return (NULL);
+	}
 
 	new->str = strdup(str);
 
-	for (nchar = 0; str[nchar]; nchar++)
+	for (i = 0; str[i]; i++)
 		;
 
-	new->len = nchar;
+	new->len = i;
 	new->next = *head;
 	*head = new;
 
