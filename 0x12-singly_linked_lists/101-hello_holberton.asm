68ef5e6bc8349eac212b57eadd7c5071f7c985f7
Ponmile Daniel <pomiledaniel@gmail.com>
2023-08-25 16:24:20 -0700

    lowlevel

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,19 @@
 section .data
     hello_msg db "Hello, Holberton", 0
-    format db "%s\n", 0
+    format db "%s", 10, 0 ; "%s" is the format for the string, 10 is the newline, 0 terminates the format string
 
 section .text
     global main
     extern printf
 
 main:
-    mov rdi, format   
-    mov rsi, hello_msg 
+    mov rdi, format
+    mov rsi, hello_msg
     xor rax, rax
     call printf
-    
-    xor rdi, rdi     
-    mov rax, 60      
+
+    xor rdi, rdi      ; Clear RDI for the return value (0)
+    mov rax, 60       ; System call number for exit
     syscall
+
 
