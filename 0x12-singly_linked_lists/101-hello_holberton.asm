f6a2488dda1029a9a8ff13a597626d73488df758
Ponmile Daniel <pomiledaniel@gmail.com>
2023-08-25 16:16:04 -0700

    low

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,18 @@
+section .data
+    hello_msg db "Hello, Holberton", 0
+    format db "%s\n", 0
+
+section .text
+    global main
+    extern printf
+
+main:
+    mov rdi, format   
+    mov rsi, hello_msg 
+    xor rax, rax
+    call printf
+    
+    xor rdi, rdi     
+    mov rax, 60      
+    syscall
+
