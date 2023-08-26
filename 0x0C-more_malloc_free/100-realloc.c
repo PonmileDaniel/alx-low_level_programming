546bbffe3b70ffd9d6267d5e1aaa6f8b6cff6d9e
Ponmile Daniel <pomiledaniel@gmail.com>
2023-08-26 13:39:47 -0700

    update

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,1 +1,38 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _realloc - This reallocates a memory block
+ * @old_size: size, in bytes of the allocated space
+ * @ptr: Pointe
+ * @new_size: New sizw, in bytes of tje new memory block
+ * Return: pointer
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+	}
+	if (old_size == new_size)
+	{
+		return (NULL);
+	}
+	free(ptr);
+	ptr = malloc(new_size);
+
+	return (ptr);
+}
