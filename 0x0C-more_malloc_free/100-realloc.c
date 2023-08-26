a4cb9790ba79afeb36064b62c1fb047a6a688dba
Ponmile Daniel <pomiledaniel@gmail.com>
2023-08-26 13:43:16 -0700

    addig

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -27,9 +27,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		ptr = malloc(new_size);
 	}
-	if (old_size == new_size)
+	if (new_size == old_size)
 	{
-		return (NULL);
+		return (ptr);
 	}
 	free(ptr);
 	ptr = malloc(new_size);
