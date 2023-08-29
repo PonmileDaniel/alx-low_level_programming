23209fa3bced31869de8793d417ad98d3eb26bf9
Ponmile Daniel <pomiledaniel@gmail.com>
2023-08-29 13:01:38 -0700

    return number

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * listint_len - Return the no of element
+ * @h: linked list
+ * 
+ * Return: number of the nodes
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t node = 0;
+
+	while (h)
+	{
+		h = h->next;
+		node++;
+	}
+
+
+	return (node);
+}
