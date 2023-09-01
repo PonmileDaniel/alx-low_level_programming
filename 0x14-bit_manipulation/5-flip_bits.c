9cf8cf7b0cc14e5321460421f5bde4390a5574dd
Ponmile Daniel <pomiledaniel@gmail.com>
2023-09-01 14:01:30 -0700

    ll

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * flip_bits - changes the values\
+ * @n: Bits
+ * @m: number 2
+ * Return: Number of bits
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int b;
+
+	for (b = 0; n || m; n >>= 1, m >>= 1)
+	{
+		if ((n & 1) != (m & 1))
+			b++;
+	}
+	return (b);
+}
+
