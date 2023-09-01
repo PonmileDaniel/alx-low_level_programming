3f9a473ab51306afd00f70a5462c36ba887bbc7a
Ponmile Daniel <pomiledaniel@gmail.com>
2023-09-01 12:53:21 -0700

    bin

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * get_bit - This return the valuee of a bit
+ * @index: The index
+ * Return: value of bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit;
+
+	if (index > 63)
+	{
+		return (-1);
+	}
+	bit = (n >> index) & 1;
+	return (bit);
+}
