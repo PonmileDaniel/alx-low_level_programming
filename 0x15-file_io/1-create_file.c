e3178db24d489c813a87acd8b5c4e6085475532d
Ponmile Daniel <pomiledaniel@gmail.com>
2023-09-05 14:11:16 -0700

    put

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * create_file - create a file
+ * @filename: a pointer the file
+ * @text_content: A pointer to a string
+ * Return: If the func fails -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int a, b, length = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (length = 0; text_content[length];)
+			length++;
+	}
+
+	a = open(filename, 0_CREAT | 0_RDWR | 0_TRUNC, 0600);
+	b = write(a, text_content, length);
+
+	if (a == -1 || b == -1)
+		return (-1);
+
+	close(a);
+
+	return (1);
+}
