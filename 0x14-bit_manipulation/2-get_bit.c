9e132c527a0cee58c682cbb4f0ef5caf35d1d7d1
PonmileDaniel <pomiledaniel@gmail.com>
2023-10-26 09:08:46 -0700

    Binary

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * get_bit - The func return the value of a bit at a given index
+ * @index: Index
+ * @n: The number
+ * Return: Integer 1 || 0
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i;
+	if (index >= (sizeof(unsigned long int) * 8))
+	{
+		return (-1);
+	}
+	for (i = 0; i < index; i++)
+	{
+		n = n >> 1;
+	}
+	return ((n & 1));
+}
