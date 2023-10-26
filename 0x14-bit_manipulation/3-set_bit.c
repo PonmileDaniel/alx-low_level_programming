173a2444da74985169596947574a7be1d0cef8f0
PonmileDaniel <pomiledaniel@gmail.com>
2023-10-26 11:11:14 -0700

    set the value of bit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * set_bit - set the bit of a number to the vslue of 1
+ * @n: points to the bit to be manipulated
+ * @index: position of the bit
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int i;
+
+	if (index >= (sizeof(unsigned long int) * 8))
+	{
+		return (-1);
+	}
+	i = 1;
+	i = i << index;
+	*n = ((*n) | i);
+	return (1);
+}
