4d13530906cf9c9e5378888e312f00ce3b63125c
PonmileDaniel <pomiledaniel@gmail.com>
2023-10-27 00:50:46 -0700

    bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -5,6 +5,7 @@
  * clear_bit - Func that set the value of a bit to zero
  * @n: Pointer to the number
  * @index: Position to change
+ * Return: RETURN -1 or 1
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
