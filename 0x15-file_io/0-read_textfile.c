98de3c75348ea64acdb0b93e07c2d8b9790cb98c
PonmileDaniel <pomiledaniel@gmail.com>
2023-10-30 09:56:49 -0700

    File i/o

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * read_textfile - func that reads a trxt file and prints it
+ * filename: pointer to a string
+ * letters: number of letters printed
+ * Return: number of letter printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int filedesc;
+	ssize_t numread, numwritten;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	filedesc = open(filename, O_RDONLY);
+
+	if (filedesc == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (!buffer)
+		return (0);
+
+	numread = read(filedesc, buffer, letters);
+	numwritten = write(STDOUT_FILENO, buffer, numread);
+
+	close(filedesc);
+	free(buffer);
+	return (numwritten);
+}
