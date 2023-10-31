1c4e27ccda7ccfb20c4619fa1b4e5f3574d4ee19
PonmileDaniel <pomiledaniel@gmail.com>
2023-10-30 22:38:47 -0700

    create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * create_file - create a file and copy
+ * @filename: The filename to be created
+ * @text_content: Content to be copied
+ * Return: 1 if success -1 if failed
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int filedesc;
+	int letterno;
+	int w;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (letterno = 0; text_content[letterno];)
+			letterno++;
+	}		
+
+	filedesc = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(filedesc, text_content, letterno);
+
+	if (w == -1)
+		return (-1);
+	close(filedesc);
+	return(1);
+}
+
