02fa876c3279208ccfe9545d5a813a06442672d6
PonmileDaniel <pomiledaniel@gmail.com>
2023-10-30 22:55:00 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,11 +11,11 @@ int create_file(const char *filename, char *text_content)
 	int letterno;
 	int w;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 	filedesc = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (text_content != NULL)
+	if (text_content == NULL)
 		text_content = "";
 
 	for (letterno = 0; text_content[letterno]; letterno++)
@@ -25,6 +25,7 @@ int create_file(const char *filename, char *text_content)
 	if (w == -1)
 		return (-1);
 	close(filedesc);
+
 	return (1);
 }
 
