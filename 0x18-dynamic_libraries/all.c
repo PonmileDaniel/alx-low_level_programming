fc16f9184e7f890bef279e0a2676c4fce1aa775c
PonmileDaniel <pomiledaniel@gmail.com>
2023-12-13 08:57:49 +0100

    removing stadlib

---
+++ 0x18-dynamic_libraries/all.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+/**
+* mod - two int
+* @a: first int
+* @b: second int
+* Return: final answer
+*/
+int mod(int a, int b)
+{
+    return (a % b);
+}
+/**
+* div - div two int
+* @a: first int
+* @b: second int
+* Return: addition
+*/
+int div(int a, int b)
+{
+    return (a / b);
+}
+/**
+* mul - multiply two int
+* @a: first int
+* @b: second int
+* Return: addition
+*/
+int mul(int a, int b)
+{
+    return (a * b);
+}
+/**
+* sub - sub two int
+* @a: first int
+* @b: second int
+* Return: subtract
+*/
+int sub(int a, int b)
+{
+    return (a - b);
+}
+/**
+* add - add two int
+* @a: first int
+* @b: second int
+* Return: subtract
+*/
+int add(int a, int b)
+{
+    return (a + b);
+}
