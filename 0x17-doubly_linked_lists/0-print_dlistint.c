34cd2b63461ecd7b1607ef6c50ba07296c7036e4
root <pomiledaniel@gmail.com>
2024-01-26 04:01:41 -0800

    Betty

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -6,16 +6,17 @@
 */
 size_t print_dlistint(const dlistint_t *h)
 {
-    int count;
-    count = 0;
+	int count;
 
-    if (h == NULL)
-        return (count);
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        count++;
-        h = h->next;
-    }
-    return (count);
+	count = 0;
+
+	if (h == NULL)
+		return (count);
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
 }
