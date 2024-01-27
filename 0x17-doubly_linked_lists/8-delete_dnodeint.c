68cf97e26fad9f057ceb6caa2ee35f0dafe1c250
root <pomiledaniel@gmail.com>
2024-01-26 17:08:53 -0800

    link

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ *
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *curr = *head;
+	unsigned int i;
+
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+
+	if (index == 0)
+	{
+		*head = curr->next;
+
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(curr);
+		return (1);
+	}
+	for (i = 0; i < index && curr != NULL; i++)
+	{
+		curr = curr->next;
+	}
+	if (curr == NULL)
+	{
+		return (-1);
+	}
+	if (curr->prev != NULL)
+	{
+		curr->next->prev = curr->prev;
+	}
+	free(curr);
+	return (1);
+}
