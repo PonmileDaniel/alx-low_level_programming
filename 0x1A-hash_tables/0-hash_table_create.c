3c66beaccde13cfaf445ba278f394b32ee85ba87
PonmileDaniel <pomiledaniel@gmail.com>
2024-04-15 02:29:53 +0100

    A functon that create a Hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Create a hash table with a given size
+ * @size: size of the hash table
+ * Return: Created hash table, or NULL  if func fails
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+        hash_table_t *new_table;
+        hash_node_t **array;
+        unsigned long int i;
+
+        new_table = malloc(sizeof(hash_table_t));
+        if (new_table == NULL)
+                return NULL;
+
+        array = malloc(sizeof(hash_node_t *) * size);
+        if (array == NULL)
+        {
+                return NULL;
+        }
+
+        for (i = 0; i < size; i++)
+                array[i] = NULL;
+
+        new_table->array = array;
+        new_table->size = size;
+
+        return (new_table);
+}
