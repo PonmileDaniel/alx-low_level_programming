62fba39625f448f66a4a849cb9fd5c4c3233de68
Ndukwe Daniel <pomiledaniel@gmail.com>
2024-04-14 19:28:04 -0700

    Betty issues

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,25 +7,24 @@
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-        hash_table_t *new_table;
-        hash_node_t **array;
-        unsigned long int i;
+	hash_table_t *new_table;
+	hash_node_t **array;
+	unsigned long int i;
 
-        new_table = malloc(sizeof(hash_table_t));
-        if (new_table == NULL)
-                return NULL;
+	new_table = malloc(sizeof(hash_table_t));
+	if (new_table == NULL)
+		return (NULL);
 
-        array = malloc(sizeof(hash_node_t *) * size);
-        if (array == NULL)
-        {
-                return NULL;
-        }
+	array = malloc(sizeof(hash_node_t *) * size);
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+		array[i] = NULL;
 
-        for (i = 0; i < size; i++)
-                array[i] = NULL;
+	new_table->array = array;
+	new_table->size = size;
 
-        new_table->array = array;
-        new_table->size = size;
-
-        return (new_table);
+	return (new_table);
 }
